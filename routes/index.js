4e279b03970b4921a82c2802dd05d7b2ba6c10f4
Gyujin Cho <och1939420@gmail.com>
2020-11-17 01:01:21 +0900

    make header.ejs

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.use('/test', testRouter);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'TUCE' });
 });
 
 module.exports = router;
