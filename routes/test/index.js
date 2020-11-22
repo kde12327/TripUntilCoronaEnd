0c0980975e75329459ca6f90e2eb314d8e8ff123
DESKTOP-0BSVB30\kde12 <kde12327@gmail.com>
2020-11-22 16:43:14 +0900

    divide test.ejs to test1,2.

--- routes/test/index.js
+++ routes/test/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/1', function(req, res, next) {
   res.render('test', { title: 'Test Page' });
+});
+
+/* GET home page. */
+router.get('/2', function(req, res, next) {
+  res.render('test2', { title: 'Test Page' });
 });
 
 module.exports = router;
