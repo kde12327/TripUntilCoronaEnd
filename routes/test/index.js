2a7f4ba70059f39ad7104623fa0ac67ceed6ebfb
DESKTOP-0BSVB30\kde12 <kde12327@gmail.com>
2020-11-23 01:26:04 +0900

    1. add side tab

--- routes/test/index.js
+++ routes/test/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/1', function(req, res, next) {
-  res.render('test', { title: 'Test Page' });
+  res.render('test', { 
+    title: 'Test Page', 
+    data: {
+      course_title: '코스 명',
+      course_list: [
+        {
+          courseItemName: '1번',
+        },
+        {
+          courseItemName: '2번',
+        },
+        {
+          courseItemName: '3번',
+        }
+      ]
+    } 
+  });
 });
 
 /* GET home page. */
