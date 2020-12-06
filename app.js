c2d70a07b571d667bf8c8eb3070c654e0558b9fb
Gyujin Cho <och1939420@gmail.com>
2020-12-06 22:37:10 +0900

    make login.ejs and register.ejs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var favicon = require('serve-favicon');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
--- routes/index.js
+++ routes/index.js
@@ -12,5 +12,11 @@ router.get('/', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'TUCE' });
 });
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'TUCE' });
+});
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'TUCE' });
+});
 
 module.exports = router;
